582b18110ff91041f608906f0c3318712e72cfe2
jiayouzhao <jiayouzhaodaye@163.com>
2021-06-27 18:45:01 +0800

    button组件

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,12 +1,40 @@
 <template>
-  <button>按钮</button>
+  <button class="i-button">
+    按钮
+  </button>
 </template>
 
 <script>
 export default {
-	name: "GButton",
+	
 };
 </script>
 
 <style lang="scss" scoped>
+.i-button{
+    --button-height:32px;
+    --font-size:14px;
+    --button-bg:white;
+    --button-active-bg:#eee;
+    --border-radius:4px;
+    --color:#333;
+    --border-color:#999;
+    --border-color-hover:#666;
+}
+.i-button{
+    font-size:var(--font-size);
+    height:var(--button-height);
+    background:var(--button-bg);
+    color:var(--border-color);
+    border:1px solid  var(--border-color);
+    border-radius:var(--border-radius);
+    padding:0px 1em;
+    line-height:var(--button-height);
+    &:hover{
+        border-color:var(--border-color-hover);
+    }
+    &:active{
+        background-color:var(--button-active-bg);
+    }
+}
 </style>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import Vue from "vue";
 import Button from "./components/Button.vue";
+import "./style/reset.scss";
+import "./style/index.scss";
 
 Vue.component("GButton", Button);
 
 new Vue({
-	el: "#app",
-	created() {
-		
-	}
+	el: "#app"
 });
