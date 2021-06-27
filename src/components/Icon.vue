a2012ac0b319fb5db1c4873658bfeb874ac8eb4d
jiayouzhao <jiayouzhaodaye@163.com>
2021-06-28 00:45:00 +0800

    抽离Icon组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg class="i-icon">
+    <use :xlink:href="`#icon-${name}`" />
+  </svg>
+</template>
+
+<script>
+export default {
+	props:[ "name" ]
+};
+</script>
+
+<style lang="scss" scoped>
+.i-icon {
+    width: 1em;
+    height: 1em;
+}
+</style>
