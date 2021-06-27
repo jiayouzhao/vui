5de2d6ae12d04fb9afbb9c8fb26d9f1e52d08198
jiayouzhao <jiayouzhaodaye@163.com>
2021-06-28 01:04:19 +0800

    Icon组件

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import Vue from "vue";
 import Button from "./components/Button.vue";
 import "./style/reset.scss";
 import "./style/index.scss";
+import Icon from "./components/Icon.vue";
 
 Vue.component("GButton", Button);
+Vue.component("IIcon", Icon);
 
 new Vue({
 	el: "#app"
