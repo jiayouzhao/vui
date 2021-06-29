020599eca9507e03d98706f59ac949e1a6ff1d1d
jiayouzhao <jiayouzhaodaye@163.com>
2021-06-29 20:02:51 +0800

    input组件

---
+++ src/components/input.vue
@@ -0,0 +1,89 @@
+<template>
+  <div
+    class="inputWrapper"
+    :class="{error}"
+  >
+    <input
+      :value="value"
+      type="text"
+      class="i-input"
+      :disabled="disabled"
+      :readonly="readonly"
+    >
+    <template v-if="error">
+      <i-icon name="error" />
+      <span class="errorMessage">{{ error }}</span>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+	props:{
+		value:{
+			type:String
+		},
+		disabled:{
+			type:Boolean,
+			default:false
+		},
+		readonly:{
+			type:Boolean,
+			default:false
+		},
+		error:{
+			type:String,
+			defalut:""
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+$border-color:#999999;
+$border-hover:#666666;
+$border-radius:4px;
+$red:#F35B54;
+$font-size:14px;
+$line-height:24px;
+.inputWrapper{  
+    display:inline-block;
+    font-size:$font-size;
+
+    &.error{
+        display:flex;
+        align-items:center;
+        color:$red;
+        .i-input{
+            border-color:$red;
+        }
+        svg{
+            margin:0px 2px;
+            fill:$red;
+        }
+        .errorMessage{
+            
+        }
+    }
+    .i-input{
+        border:1px solid $border-color;
+        font-size:$font-size;
+        line-height:$line-height;
+        padding:0px 8px;
+        border-radius:$border-radius;
+        &:hover{
+            border-color:$border-hover;
+        }
+        &:focus{
+            box-shadow:inset 0px 1px 3px rgba(0,0,0,0.5);
+            outline:none;
+        }
+        &:disabled,&:read-only{
+            border-color:#aaa;
+            color:#aaa;
+            cursor:not-allowed;
+        }
+    }
+}
+
+</style>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import "./style/reset.scss";
 import "./style/index.scss";
 import Icon from "./components/Icon.vue";
 import ButtonGroup from "./components/button-group.vue";
+import Input from "./components/input.vue";
 
 Vue.component("GButton", Button);
 Vue.component("IIcon", Icon);
 Vue.component("ButtonGroup", ButtonGroup);
+Vue.component("IInput", Input);
 
 new Vue({
 	el: "#app"
